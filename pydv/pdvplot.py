b64971de50ac7f1a4ff3898994320d20a255613a
Kevin Griffin <griffin28@llnl.gov>
2019-05-02 14:12:44 -0700

    Resolves #31. Updated the version and release date. (#32)
    * Updated copyright, release date, release version, typos (#20)
    
    * Updated docs for 2.4.2
    
    * Resolves #27. Gave low and high default values if there were none.
    
    * Resolves #28. Updated precmd to parse math operations correctly. Also
    updated the test cmds file.
    
    * Resolves #31. Updated the version and release date.

--- pydv/pdvplot.py
+++ pydv/pdvplot.py
@@ -111,7 +111,7 @@ class Plotter(QMainWindow):
         here = path.abspath(path.dirname(__file__))
 
         # Setup Application
-        self.setWindowTitle('Python Data Visualizer 2.4.2')
+        self.setWindowTitle('Python Data Visualizer 2.4.2.1')
         self.setWindowIcon(QIcon(path.join(here, 'img/app_icon3.png')))
         self._pydvcmd = pydvcmd
 
@@ -512,7 +512,7 @@ class Plotter(QMainWindow):
 
     def __aboutPyDV(self):
         QMessageBox.about(self, self.tr('About PyDV'), self.tr('<h2>About PyDV</h2>'
-                                                               '<p style="font-family:courier; font-size:40%;">version 2.4.2</p>'
+                                                               '<p style="font-family:courier; font-size:40%;">version 2.4.2.1</p>'
                                                                '<p style="font-family:verdana;"><a href="https://lc.llnl.gov/confluence/display/PYDV/PyDV%3A+Python+Data+Visualizer">PyDV</a> is a 1D graphics tool, heavily based on the ULTRA plotting tool.</p>'
                                                                '<p style="font-family:courier; font-size:-1;">Copyright &copy; 2011-2019, Lawrence Livermore National Security, LLC.</p>'
                                                                '<p style="font-family:veranda; font-size:80%;">Written by: Mason Kwiat, Douglas S. Miller, and Kevin Griffin</p>'
