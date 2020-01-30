785ebf2c217e998c1d7da62a72a9729cf08e87b8
Kevin Griffin <griffin28@llnl.gov>
2020-01-30 14:05:32 -0800

    Added the RTD theme for the docs

--- pydv/docs/conf.py
+++ pydv/docs/conf.py
@@ -32,6 +32,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -113,7 +114,7 @@ numfig = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
