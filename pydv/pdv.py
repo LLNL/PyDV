2810b27c95ca0901701dca01d75dcdc4103be6fe
Kevin Griffin <griffin28@llnl.gov>
2020-08-06 12:09:08 -0700

    Resolves #144. Minor updates to copyright year and version.

--- pydv/pdv.py
+++ pydv/pdv.py
@@ -6168,7 +6168,7 @@ For a painfully complete explanation of the regex syntax, type 'help regex'.
 
     def console_run(self):
         while True:
-            self.cmdloop('\n\tPython Data Visualizer 3.0.1  -  05.15.2020\n\tType "help" for more information.\n\n')
+            self.cmdloop('\n\tPython Data Visualizer 3.0.2  -  0x.xx.2020\n\tType "help" for more information.\n\n')
             print('\n   Starting Python Console...\n   Ctrl-D to return to PyDV\n')
             console = code.InteractiveConsole(locals())
             console.interact()
