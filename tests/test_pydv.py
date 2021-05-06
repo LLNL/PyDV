1d5507ee4ea6dc2efde28bbbc031c049eaa6b6c4
Eddie Rusu <rusu1@llnl.gov>
2021-05-06 09:26:20 -0700

    Image testing against baseline

---
+++ tests/test_pydv.py
@@ -0,0 +1,273 @@
+
+from matplotlib import image
+from numpy import testing as np
+
+def test_image_01():
+    baseline = image.imread('baseline/test_image_01.png')
+    output = image.imread('output/test_image_01.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_02():
+    baseline = image.imread('baseline/test_image_02.png')
+    output = image.imread('output/test_image_02.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_03():
+    baseline = image.imread('baseline/test_image_03.png')
+    output = image.imread('output/test_image_03.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_04():
+    baseline = image.imread('baseline/test_image_04.png')
+    output = image.imread('output/test_image_04.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_05():
+    baseline = image.imread('baseline/test_image_05.png')
+    output = image.imread('output/test_image_05.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_06():
+    baseline = image.imread('baseline/test_image_06.png')
+    output = image.imread('output/test_image_06.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_07():
+    baseline = image.imread('baseline/test_image_07.png')
+    output = image.imread('output/test_image_07.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_08():
+    baseline = image.imread('baseline/test_image_08.png')
+    output = image.imread('output/test_image_08.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_09():
+    baseline = image.imread('baseline/test_image_09.png')
+    output = image.imread('output/test_image_09.png')
+    np.assert_equal(baseline, output)
+
+def test_image_10():
+    baseline = image.imread('baseline/test_image_10.png')
+    output = image.imread('output/test_image_10.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_11():
+    baseline = image.imread('baseline/test_image_11.png')
+    output = image.imread('output/test_image_11.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_12():
+    baseline = image.imread('baseline/test_image_12.png')
+    output = image.imread('output/test_image_12.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_13():
+    baseline = image.imread('baseline/test_image_13.png')
+    output = image.imread('output/test_image_13.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_14():
+    baseline = image.imread('baseline/test_image_14.png')
+    output = image.imread('output/test_image_14.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_15():
+    baseline = image.imread('baseline/test_image_15.png')
+    output = image.imread('output/test_image_15.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_16():
+    baseline = image.imread('baseline/test_image_16.png')
+    output = image.imread('output/test_image_16.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_17():
+    baseline = image.imread('baseline/test_image_17.png')
+    output = image.imread('output/test_image_17.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_18():
+    baseline = image.imread('baseline/test_image_18.png')
+    output = image.imread('output/test_image_18.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_19():
+    baseline = image.imread('baseline/test_image_19.png')
+    output = image.imread('output/test_image_19.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_20():
+    baseline = image.imread('baseline/test_image_20.png')
+    output = image.imread('output/test_image_20.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_21():
+    baseline = image.imread('baseline/test_image_21.png')
+    output = image.imread('output/test_image_21.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_22():
+    baseline = image.imread('baseline/test_image_22.png')
+    output = image.imread('output/test_image_22.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_23():
+    baseline = image.imread('baseline/test_image_23.png')
+    output = image.imread('output/test_image_23.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_24():
+    baseline = image.imread('baseline/test_image_24.png')
+    output = image.imread('output/test_image_24.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_25():
+    baseline = image.imread('baseline/test_image_25.png')
+    output = image.imread('output/test_image_25.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_26():
+    baseline = image.imread('baseline/test_image_26.png')
+    output = image.imread('output/test_image_26.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_27():
+    baseline = image.imread('baseline/test_image_27.png')
+    output = image.imread('output/test_image_27.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_28():
+    baseline = image.imread('baseline/test_image_28.png')
+    output = image.imread('output/test_image_28.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_29():
+    baseline = image.imread('baseline/test_image_29.png')
+    output = image.imread('output/test_image_29.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_30():
+    baseline = image.imread('baseline/test_image_30.png')
+    output = image.imread('output/test_image_30.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_31():
+    baseline = image.imread('baseline/test_image_31.png')
+    output = image.imread('output/test_image_31.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_32():
+    baseline = image.imread('baseline/test_image_32.png')
+    output = image.imread('output/test_image_32.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_33():
+    baseline = image.imread('baseline/test_image_33.png')
+    output = image.imread('output/test_image_33.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_34():
+    baseline = image.imread('baseline/test_image_34.png')
+    output = image.imread('output/test_image_34.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_35():
+    baseline = image.imread('baseline/test_image_35.png')
+    output = image.imread('output/test_image_35.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_36():
+    baseline = image.imread('baseline/test_image_36.png')
+    output = image.imread('output/test_image_36.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_37():
+    baseline = image.imread('baseline/test_image_37.png')
+    output = image.imread('output/test_image_37.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_38():
+    baseline = image.imread('baseline/test_image_38.png')
+    output = image.imread('output/test_image_38.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_39():
+    baseline = image.imread('baseline/test_image_39.png')
+    output = image.imread('output/test_image_39.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_40():
+    baseline = image.imread('baseline/test_image_40.png')
+    output = image.imread('output/test_image_40.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_41():
+    baseline = image.imread('baseline/test_image_41.png')
+    output = image.imread('output/test_image_41.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_42():
+    baseline = image.imread('baseline/test_image_42.png')
+    output = image.imread('output/test_image_42.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_43():
+    baseline = image.imread('baseline/test_image_43.png')
+    output = image.imread('output/test_image_43.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_44():
+    baseline = image.imread('baseline/test_image_44.png')
+    output = image.imread('output/test_image_44.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_45():
+    baseline = image.imread('baseline/test_image_45.png')
+    output = image.imread('output/test_image_45.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_46():
+    baseline = image.imread('baseline/test_image_46.png')
+    output = image.imread('output/test_image_46.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_47():
+    baseline = image.imread('baseline/test_image_47.png')
+    output = image.imread('output/test_image_47.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_48():
+    baseline = image.imread('baseline/test_image_48.png')
+    output = image.imread('output/test_image_48.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_49():
+    baseline = image.imread('baseline/test_image_49.png')
+    output = image.imread('output/test_image_49.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_50():
+    baseline = image.imread('baseline/test_image_50.png')
+    output = image.imread('output/test_image_50.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_51():
+    baseline = image.imread('baseline/test_image_51.png')
+    output = image.imread('output/test_image_51.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_52():
+    baseline = image.imread('baseline/test_image_52.png')
+    output = image.imread('output/test_image_52.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_53():
+    baseline = image.imread('baseline/test_image_53.png')
+    output = image.imread('output/test_image_53.png')
+    np.assert_equal(baseline, output)
+        
+def test_image_54():
+    baseline = image.imread('baseline/test_image_54.png')
+    output = image.imread('output/test_image_54.png')
+    np.assert_equal(baseline, output)
