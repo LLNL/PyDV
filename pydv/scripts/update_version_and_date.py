1a2804783dddb00d599642676b10ac5ca25702d3
Eddie Rusu <rusu1@llnl.gov>
2021-05-06 21:26:33 -0700

    Update the date too

--- pydv/scripts/update_version_and_date.py
+++ pydv/scripts/update_version_and_date.py
@@ -1,4 +1,5 @@
 
+from datetime import date
 import os
 import argparse
 import fileinput
@@ -6,8 +7,6 @@ import fileinput
 SCRIPTS_DIR = os.path.dirname(os.path.abspath(__file__))
 SOURCE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-print(ROOT_DIR)
 
 # Get the version number from the version file
 version_file = os.path.join(SCRIPTS_DIR, 'version.txt')
@@ -39,20 +38,29 @@ files = [
     os.path.join(SOURCE_DIR, 'docs', 'conf.py'),
     os.path.join(SCRIPTS_DIR, 'version.txt'),
 ]
-for file in files:
-    with fileinput.FileInput(file, inplace=True) as file:
-        for line in file:
+for afile in files:
+    with fileinput.FileInput(afile, inplace=True) as fp:
+        for line in fp:
             print(line.replace(version, new_version), end='')
 
 # Special case: the conf.py file also wants a representation of just major.minor
-with fileinput.FileInput(os.path.join(SOURCE_DIR, 'docs', 'conf.py'), inplace=True) as file:
-    for line in file:
-        
+with fileinput.FileInput(os.path.join(SOURCE_DIR, 'docs', 'conf.py'), inplace=True) as fp:
+    for line in fp:
         print(line.replace(f'{version_list[0]}.{version_list[1]}', f'{major}.{minor}'), end='')
 
 # TODO: Add section on Release notes for PyDV new_version to the release notes
 # file
 
-print(new_version)
-x = []
-
+# Replace the date
+with open(os.path.join(SCRIPTS_DIR, 'date.txt')) as fp:
+    old_date = fp.read()
+new_date = date.today()
+new_date = f'{new_date.month:02d}.{new_date.day:02d}.{new_date.year:04d}'
+files = [
+    os.path.join(SOURCE_DIR, 'pdv.py'),
+    os.path.join(SCRIPTS_DIR, 'date.txt'),
+]
+for afile in files:
+    with fileinput.FileInput(afile, inplace=True) as fp:
+        for line in fp:
+            print(line.replace(old_date, new_date), end='')
