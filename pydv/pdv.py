8c59741812a3a062b79ebc389e20887d032300eb
Eddie Rusu <rusu1@llnl.gov>
2022-04-20 09:20:49 -0700

    Version 3.1.0

--- pydv/pdv.py
+++ pydv/pdv.py
@@ -6680,7 +6680,7 @@ For a painfully complete explanation of the regex syntax, type 'help regex'.
 
     def console_run(self):
         while True:
-            self.cmdloop('\n\tPython Data Visualizer 3.0.7  -  04.19.2022\n\tType "help" for more information.\n\n')
+            self.cmdloop('\n\tPython Data Visualizer 3.1.0  -  04.20.2022\n\tType "help" for more information.\n\n')
             print('\n   Starting Python Console...\n   Ctrl-D to return to PyDV\n')
             console = code.InteractiveConsole(locals())
             console.interact()
