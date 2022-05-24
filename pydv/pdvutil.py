2ea2e904d95dd92722405ed7e19029a1f60fce12
Edward Rusu <rusu1@llnl.gov>
2022-05-23 18:07:33 -0700

    At indexing changed to minus instead of plus (#266)
    * At indexing changed to minus instead of plus

--- pydv/pdvutil.py
+++ pydv/pdvutil.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2011-2020, Lawrence Livermore National Security, LLC.
+# Copyright (c) 2011-2022, Lawrence Livermore National Security, LLC.
 # Produced at the Lawrence Livermore National Laboratory
 # Written by Mason Kwiat, Douglas S. Miller, and Kevin Griffin, Edward Rusu
 # e-mail: rusu1@llnl.gov
@@ -82,7 +82,7 @@ def parsemath(line, plotlist, commander, xdomain):
     for val in line:
         dex = None
         if(val[0] == '@'): # are we a curve labeled @N, i.e., beyond a-z?
-            dex = int(val[1:]) + 1
+            dex = int(val[1:]) - 1
             sendline += ' plotlist['+str(dex)+'] '
         elif(len(val) == 1 and ord(val.upper()) <= ord('Z') and ord(val.upper()) >= ord('A')): # or a curve a-z?
             dex = ord(val.upper()) - ord('A')
